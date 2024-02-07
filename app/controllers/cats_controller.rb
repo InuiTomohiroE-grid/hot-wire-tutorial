c128c58df9efc332678354a7741a39ecfbd06f70
root <root@EG181>
2024-02-07 16:56:48 +0900

    first

---
+++ app/controllers/cats_controller.rb
@@ -0,0 +1,63 @@
+class CatsController < ApplicationController
+  before_action :set_cat, only: %i[ show edit update destroy ]
+
+  # GET /cats or /cats.json
+  def index
+    @search = Cat.ransack(params[:q])
+    @search.sorts = 'id desc' if @search.sorts.empty?
+    @cats = @search.result.page(params[:page])
+    @cats_count = Cat.count
+  end
+
+  # GET /cats/1 or /cats/1.json
+  def show
+  end
+
+  # GET /cats/new
+  def new
+    @cat = Cat.new
+  end
+
+  # GET /cats/1/edit
+  def edit
+  end
+
+  # POST /cats or /cats.json
+  def create
+    @cat = Cat.new(cat_params)
+
+    if @cat.save
+      flash.now.notice = "ねこを登録しました。"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /cats/1 or /cats/1.json
+  def update
+    if @cat.update(cat_params)
+      flash.now.notice = "ねこを更新しました。"
+    else
+      # バリデーションエラー時にはedit.html.erbをrenderする
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /cats/1 or /cats/1.json
+  def destroy
+    @cat.destroy
+    render turbo_stream: turbo_stream.remove('cats_count', partial: 'cats/cats_count', locals: { cats_count: @cats_count })
+    flash.now.notice = "ねこを削除しました。"
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_cat
+      @cat = Cat.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def cat_params
+      params.require(:cat).permit(:name, :age)
+    end
+end
